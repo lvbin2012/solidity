699d2c3f3653bdba3b1d7c5fa0362aed8a9911fa
Alex Beregszaszi <alex@rtfs.hu>
2020-07-02 17:39:04 +0100

    Add check that regular and unicode string literals are well formatted

--- libsolidity/ast/Types.h
+++ libsolidity/ast/Types.h
@@ -628,6 +628,8 @@ public:
 	std::string toString(bool) const override;
 	TypePointer mobileType() const override;
 
+	bool isValidASCII() const;
+
 	std::string const& value() const { return m_value; }
 
 protected:
